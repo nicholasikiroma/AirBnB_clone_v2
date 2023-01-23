e62986989dfbc98d8872454eaa41285409a4a270
Nicholas <nicholasikiroma@gmail.com>
2023-01-23 20:17:11 +0100

    task 2, bug fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,24 +21,21 @@ def do_deploy(archive_path):
         # upload archive
         put(archive_path, '/tmp/')
 
-        # create target dir
         timestamp = archive_path[-18:-4]
         run('sudo mkdir -p /data/web_static/\
 releases/web_static_{}/'.format(timestamp))
 
         # uncompress archive and delete .tgz
         run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-                /data/web_static/releases/web_static_{}/'
+/data/web_static/releases/web_static_{}/'
             .format(timestamp, timestamp))
 
-        # remove archive
         run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
 
         # move contents into host web_static
         run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
 /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
 
-        # remove extraneous web_static dir
         run('sudo rm -rf /data/web_static/releases/\
 web_static_{}/web_static'
             .format(timestamp))
@@ -46,11 +43,11 @@ web_static_{}/web_static'
         # delete pre-existing sym link
         run('sudo rm -rf /data/web_static/current')
 
-        # re-establish symbolic link
+        # re-establish sym link
         run('sudo ln -s /data/web_static/releases/\
 web_static_{}/ /data/web_static/current'.format(timestamp))
     except Exception:
         return False
 
-        # return True on success
+        # True on success
         return True
